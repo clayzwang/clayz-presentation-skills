cf0f72da5236fd5983a2d00484cd95060856f98b
clayzwang <clayzwang@163.com>
2026-08-23 13:48:51 +0800

    feat(index): expose capability routing API

--- packages/index_runtime/runtime.py
+++ packages/index_runtime/runtime.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 """Compatibility imports for the index-native retrieval runtime."""
 
+from .capability import CAPABILITY_RESOLUTION_CONTRACT, mandatory_core, resolve_capabilities
 from .constants import INDEX_CONTRACT, RECEIPT_CONTRACT, REQUEST_CONTRACT, IndexRuntimeError
 from .io import read_json, write_json
 from .provider import IndexProvider
@@ -10,13 +11,16 @@ from .utils import tokenize
 from .validation import validate_record, validate_request
 
 __all__ = [
+    "CAPABILITY_RESOLUTION_CONTRACT",
     "INDEX_CONTRACT",
     "RECEIPT_CONTRACT",
     "REQUEST_CONTRACT",
     "CompositeIndex",
     "IndexProvider",
     "IndexRuntimeError",
+    "mandatory_core",
     "read_json",
+    "resolve_capabilities",
     "tokenize",
     "validate_record",
     "validate_request",
